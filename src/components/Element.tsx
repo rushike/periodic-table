5b067696d93cc98af39e115a27f5d1e12fcf3c35
rushike <rushike.ab1@gmail.com>
2023-06-10 20:57:34 +0530

    Added color to categories in periodic table

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -1,46 +1,16 @@
 import "@/assets/css/element.css"
-import { JSX } from "solid-js/jsx-runtime"
 
-const data =   {
-  "name": "Hydrogen",
-  "appearance": "colorless gas",
-  "atomic_mass": 1.008,
-  "boil": 20.271,
-  "category": "diatomic nonmetal",
-  "color": null,
-  "density": 0.08988,
-  "discovered_by": "Henry Cavendish",
-  "melt": 13.99,
-  "molar_heat": 28.836,
-  "named_by": "Antoine Lavoisier",
-  "number": 1,
-  "period": 1,
-  "phase": "Gas",
-  "source": "https://en.wikipedia.org/wiki/Hydrogen",
-  "spectral_img": "https://en.wikipedia.org/wiki/File:Hydrogen_Spectra.jpg",
-  "summary": "Hydrogen is a chemical element with chemical symbol H and atomic number 1. With an atomic weight of 1.00794 u, hydrogen is the lightest element on the periodic table. Its monatomic form (H) is the most abundant chemical substance in the Universe, constituting roughly 75% of all baryonic mass.",
-  "symbol": "H",
-  "xpos": 1,
-  "ypos": 1,
-  "shells": [
-    1
-  ],
-  "group": 1,
-  "econf": {
-    "base": "[Rn]",
-    "d": {
-      "n": "6",
-      "o": "10"
-    },
-    "s": {
-      "n": "7",
-      "o": "2"
-    },
-    "p": {
-      "n": "7",
-      "o": "6"
-    }
-  }
+const COLOR_PALETTE :  {[key: string]: string} = {
+  "alkali-metal"          : "#ff4040",
+  "alkaline-earth-metal"  : "#ffbd94",
+  "transition-metal"      : "#ffff66",
+  "post-transition-metal" : "#91ff66",
+  "metalloid"             : "#66ffcf",
+  "polyatomic-nonmetal"   : "#85cef2",
+  "diatomic-nonmetal"     : "#66c9ff",
+  "lanthanide"            : "#c966ff",
+  "actinide"              : "#f24ed1",
+  "noble-gas"             : "#9985f2"
 }
 
 export interface EConfShellType {
@@ -88,7 +58,7 @@ interface ElementCellProps {
 
 export function ElementCell({element} : ElementCellProps) {
   
-  return <div class="grid grid-auto-rows el-container px-1">
+  return <div class="grid grid-auto-rows el-container px-1" style={{"background-color" : `${COLOR_PALETTE[element.category]}`}}>
     {/* Top Header Line */}
     <div class="grid grid-cols-2">
         <div class = "el-atomic-no">
